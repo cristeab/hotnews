bcf8da6b4b06eb0a2879c0c7bb32acec50ebcfbc
Bogdan Cristea <cristeab@gmail.com>
2013-10-27 12:41:43 +0100

    corrected extension in order to check for already opened hotnews tab

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
 chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.create({'url': "http://www.hotnews.ro"});
+  chrome.tabs.query({}, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      if (0 == tabs[i].url.indexOf("http://www.hotnews.ro")) {
+        chrome.tabs.update(tabs[i].id, {selected: true});
+        break;
+      }
+    }
+    if (i == tabs.length) {
+      chrome.tabs.create({'url': "http://www.hotnews.ro"});
+    }
+  });
 });
+
